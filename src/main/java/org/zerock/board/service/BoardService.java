f9d53832e6273f111cb2249359506bb67fff18f7
oldaim <dgk0911@gmail.com>
2021-01-19 15:49:22 +0900

    1/19-2차

--- src/main/java/org/zerock/board/service/BoardService.java
+++ src/main/java/org/zerock/board/service/BoardService.java
@@ -1,15 +1,22 @@
 package org.zerock.board.service;
 
-import org.springframework.data.domain.PageRequest;
+
 import org.zerock.board.dto.BoardDTO;
 import org.zerock.board.dto.PageRequestDTO;
+import org.zerock.board.dto.PageResultDTO;
 import org.zerock.board.entity.Board;
 import org.zerock.board.entity.Member;
 
 public interface BoardService {
     Long register(BoardDTO dto); //dtoToEntity 메서드를 사용해서 데이터베이스에 dto로 전달된 내용을 entity(객체)
-                                 //로 바꿔서 저장하는 기능
-    PageResultDTO<BoardDTO,Object[]> getList(PageRequestDTO pageRequestDTO);
+                                 //로 바꿔서 저장하는 기능 ->등록
+    PageResultDTO<BoardDTO,Object[]> getList(PageRequestDTO pageRequestDTO); //페이징 처리
+
+    BoardDTO get(Long bno); //조회
+
+    void removeWithReplies(Long bno); //삭제
+
+    void modify(BoardDTO boardDTO);//수정
 
     default Board dtoToEntity(BoardDTO dto){
         Member member = Member.builder().email(dto.getWriterEmail()).build();
--- src/test/java/org/zerock/board/service/BoardServiceTests.java
+++ src/test/java/org/zerock/board/service/BoardServiceTests.java
@@ -3,7 +3,10 @@ package org.zerock.board.service;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
 import org.zerock.board.dto.BoardDTO;
+import org.zerock.board.dto.PageRequestDTO;
+import org.zerock.board.dto.PageResultDTO;
 
 @SpringBootTest
 public class BoardServiceTests {
@@ -12,7 +15,7 @@ public class BoardServiceTests {
     private BoardService boardService;
 
     @Test
-    public void testRegister(){
+    public void testRegister(){ //등록
         BoardDTO dto = BoardDTO.builder()
                 .title("Test..")
                 .content("Test..")
@@ -22,4 +25,42 @@ public class BoardServiceTests {
         Long bno =boardService.register(dto);
     }
 
+    @Test
+    public void testList(){ //페이징 처리
+        PageRequestDTO pageRequestDTO = new PageRequestDTO();
+
+        PageResultDTO<BoardDTO,Object[]> result =boardService.getList(pageRequestDTO);
+
+        for(BoardDTO boardDTO : result.getDtoList()){
+            System.out.println(boardDTO);
+        }
+    }
+
+    @Test
+    public void testGet(){ //조회
+        Long bno =100L;
+
+        BoardDTO boardDTO =boardService.get(bno);
+
+        System.out.println(boardDTO);
+    }
+
+    @Test
+    public void testRemove(){ //삭제
+        Long bno =3L;
+
+        boardService.removeWithReplies(bno);
+    }
+
+    @Test
+    public void testModify(){//수정
+
+        BoardDTO boardDTO = BoardDTO.builder()
+                .bno(2L)
+                .title("제목 변경합니다")
+                .content("내용 변경합니다")
+                .build();
+
+        boardService.modify(boardDTO);
+    }
 }
