a0e6f0784124c33b09ec3538d6d22709e71cf14c
oldaim <dgk0911@gmail.com>
2021-01-21 12:34:17 +0900

    1/21

--- src/main/java/org/zerock/board/controller/BoardController.java
+++ src/main/java/org/zerock/board/controller/BoardController.java
@@ -3,11 +3,86 @@ package org.zerock.board.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.board.dto.BoardDTO;
+import org.zerock.board.dto.PageRequestDTO;
+import org.zerock.board.service.BoardService;
+
 
 @Controller
 @RequestMapping("/board/")
 @Log4j2
 @RequiredArgsConstructor
 public class BoardController {
+    private final BoardService boardService;
+
+    @GetMapping("/list") //페이징 처리
+    public void list(PageRequestDTO pageRequestDTO , Model model){
+
+        log.info("list............"+pageRequestDTO);
+        model.addAttribute("result",boardService.getList(pageRequestDTO));
+    }
+
+    @GetMapping("/register") //게시물 등록 (get)
+    public void register(){
+        log.info("register get...");
+    }
+
+    @PostMapping("/register") //게시물 등록 (post)
+    public String registerPost(BoardDTO dto, RedirectAttributes redirectAttributes){
+        log.info("dto....."+dto);
+
+        Long bno = boardService.register(dto);
+
+        redirectAttributes.addFlashAttribute("msg",bno);
+
+        return "redirect:/board/list";
+    }
+
+    @GetMapping({"/read","/modify"})//게시물 조회,수정
+    public void read(@ModelAttribute("requestDTO") PageRequestDTO pageRequestDTO,Long bno ,Model model){
+        log.info("bno: "+bno);
+
+        BoardDTO boardDTO =boardService.get(bno);
+
+        log.info(boardDTO);
+
+        model.addAttribute("dto",boardDTO);
+    }
+
+    @PostMapping("/remove") //게시물 삭제
+    public String remove(Long bno, RedirectAttributes redirectAttributes){
+
+        log.info("bno: "+bno);
+
+        boardService.removeWithReplies(bno);
+
+        redirectAttributes.addFlashAttribute("msg",bno);
+
+        return "redirect:/board/list";
+
+    }
+
+    @PostMapping("/modify")
+    public String modify(BoardDTO dto,@ModelAttribute("requestDTO") PageRequestDTO requestDTO,
+                         RedirectAttributes redirectAttributes){
+
+        log.info("Post modify.......................................................");
+        log.info("dto: "+dto);
+
+        boardService.modify(dto);
+
+        redirectAttributes.addAttribute("page",requestDTO.getPage());
+        redirectAttributes.addAttribute("type",requestDTO.getType());
+        redirectAttributes.addAttribute("keyword",requestDTO.getKeyword());
+
+        redirectAttributes.addAttribute("bno",dto.getBno());
+
+        return "redirect:/board/read";
+    }
 }
